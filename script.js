6e0c7cabdb4052ce5a1087baea283226a1e97788
haywhyyhwyah <ayomideisrael623@gmail.com>
2025-06-17 11:20:23 +0100

    againnnnnnnnn

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function checker() {
     const num = (checkInput.value)
     if (num === '') {
-        show.innerHTML = '<p title="Error" class="alert alert-danger text-center">Please enter a number ❌</p>'
+        show.innerHTML = '<p title="Error" class="alert alert-danger text-center fs-2">Please enter a number ❌</p>'
     } else {
         if (num % 3 == 0 && num % 5 == 0) {
-            show.innerHTML = '<p class="alert alert-success text-center">'+ num +' is a Fizz-Buzz ✅</p>'
+            show.innerHTML = '<p class="alert alert-success text-center fs-2">'+ num +' is a Fizz-Buzz ✅</p>'
         }else if (num % 3 == 0) {
-            show.innerHTML = '<p class="alert alert-light text-center"> ' + num + ' is a Fizz ✅</p>'
+            show.innerHTML = '<p class="alert alert-light text-center fs-2"> ' + num + ' is a Fizz ✅</p>'
         }else if (num % 5 == 0){
-            show.innerHTML = '<p class="alert alert-warning text-center">'+ num +' is a Buzz ✅</p>'
+            show.innerHTML = '<p class="alert alert-warning text-center fs-2">'+ num +' is a Buzz ✅</p>'
         }else if (num % 3 !== 0 && num % 5 !==0){
-            show.innerHTML = '<p class="alert alert-danger text-center">'+ num +' is neither a Fizz nor Buzz ❌</p>'
+            show.innerHTML = '<p class="alert alert-danger text-center fs-2">'+ num +' is neither a Fizz nor Buzz ❌</p>'
         }
     }
 }
